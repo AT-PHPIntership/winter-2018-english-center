5e2657edc22ed09944a0001e0a95b1e1175a02ff
at-camphan <cam.phan@asiantech.vn>
2018-12-28 17:04:33 +0700

    Design form UI add User

--- resources/lang/en/user.php
+++ resources/lang/en/user.php
@@ -1,6 +1,8 @@
 <?php
 return [
-    'show_list_user' => [
+    'show_list_user' => 
+    [
+        'title' => 'List Users',
         'id' => 'ID',
         'email' => 'Email',
         'email_verified_at' => 'Email Verifiled',
@@ -8,5 +10,26 @@ return [
         'role_id' => 'Role ID',
         'created_at' => 'Create',
         'updated_at' => 'Update',
+    ],
+    'create_user' => 
+    [
+        'title' => 'Create User',
+        'name' => 'Name',
+        'email' => 'Email',
+        'password' => 'Password',
+        'confirm_pass' => 'Confirm Password',
+        'age' => 'Age',
+        'birthday' => 'Birthday',
+        'phone' => 'Phone',
+        'role' => 
+        [
+            'name' => 'Role',
+            'url' => 'Avatar',
+            'admin' => 'Admin',
+            'trial' => 'Trial',
+            'vip' => 'VIP'
+        ],
+        'url' => 'Avatar',
+        'button' => 'Create',
     ]
 ];
