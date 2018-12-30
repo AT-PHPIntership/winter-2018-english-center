afa53d9c412e0e9db36729d00a2e4daf69f22e45
at-phuongtran <phuong.tran@asiantech.vn>
2018-12-30 13:16:57 +0700

    fix cmt show list courses

--- resources/lang/en/course.php
+++ resources/lang/en/course.php
@@ -1,12 +1,13 @@
 <?php
     return [
         'title' => 'Data Course',
-        'id' => 'ID',
-        'count_view' => 'Count View',
-        'title' => 'Title',
-        'sub_course' => 'Sub Course',
-        'total_rating' => 'Total Rating',
-        'average' => 'Average',
-        'action' => 'Action',
-        'width' => '50px',
+        'list_course' => [
+            'id' => 'ID',
+            'count_view' => 'View',
+            'title' => 'Name',
+            'parent' => 'Parent',
+            'total_rating' => 'Total Rating',
+            'average' => 'Average',
+            'action' => 'Action',
+        ],
     ];
