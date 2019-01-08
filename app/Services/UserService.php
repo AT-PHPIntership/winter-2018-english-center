9d9b9f776967585d7c5fd9014f3e59648e7b55d2
at-camphan <cam.phan@asiantech.vn>
2019-01-08 09:46:32 +0700

    edit and update user in DB

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -18,6 +18,7 @@ class UserService
         $users = User::with('userProfile')->paginate(config('define.page_site'));
         return $users;
     }
+
     /**
      * Store a newly created resource in storage.
      *
@@ -30,5 +31,20 @@ class UserService
         $data['password'] = bcrypt($data['password']);
         $user = User::create($data);
         $user->userProfile()->create($data);
+    }
+
+    /**
+    * Handle update user to database
+    *
+    * @param \Illuminate\Http\Request $data data
+    * @param User                     $user user
+    *
+    * @return void
+    */
+    public function update($data, $user)
+    {
+        $data['password'] = bcrypt($data['password']);
+        $user->update($data);
+        $user->userProfile->update($data);
     }
 }
