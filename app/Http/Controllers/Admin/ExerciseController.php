5662b9fe1b1754dea85f24300761d298d6838a54
at-phuongtran <phuong.tran@asiantech.vn>
2019-01-15 13:44:09 +0700

    Show detail exercise

--- app/Http/Controllers/Admin/ExerciseController.php
+++ app/Http/Controllers/Admin/ExerciseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\ExerciseService;
+use App\Models\Exercise;
 
 class ExerciseController extends Controller
 {
@@ -17,5 +18,18 @@ class ExerciseController extends Controller
     {
         $exercises = app(ExerciseService::class)->index();
         return view('backend.exercises.index')->with('exercises', $exercises);
+    }
+
+    /**
+      * Show the form for creating a new resource.
+      *
+      * @param Exercise $exercise exercise
+      *
+      * @return \Illuminate\Http\Response
+     */
+    public function show(Exercise $exercise)
+    {
+        $exercises = app(ExerciseService::class)->show($exercise);
+        return view('backend.exercises.show')->with('exercises', $exercises);
     }
 }
---
+++ resources/lang/en/question.php
@@ -0,0 +1,10 @@
+<?php
+    return [
+        'title' => 'List Question',
+        'list_question' => [
+            'id' => 'ID',
+            'question' => ' Question',
+            'answer' => 'Answer',
+            'action' => 'Action',
+        ],
+    ];
