2a88770b4085150b4fdc1a64dccd2bfe7c8339cd
at-camphan <cam.phan@asiantech.vn>
2019-02-25 23:32:07 +0700

    fix convention

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -16,7 +16,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return $next('admin/login');
+            return route('admin/login');
         }
     }
 }
